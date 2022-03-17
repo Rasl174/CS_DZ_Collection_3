7ac7df430d66af173b010f5adf94349f0ce05cb5
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-17 16:08:27 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_Collection_3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_Collection_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            bool canExit = false;
+
+            Console.WriteLine("Для сложения введите sum. Для выхода введите exit.");
+
+            while (!canExit)
+            {
+                Console.Write("Введите числa которые нужно сложить , sum или exit: ");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "sum":
+                        int numbersSum = 0;
+
+                        foreach (var item in numbers)
+                        {
+                            numbersSum += item;
+                        }
+
+                        Console.Clear();
+                        Console.WriteLine("Сумма: " + numbersSum);
+                        break;
+
+                    case "exit":
+
+                        canExit = true;
+                        break;
+
+                }
+
+                if (int.TryParse(userInput, out int result))
+                {
+                    numbers.Add(result);
+                }
+            }
+        }
+    }
+}
